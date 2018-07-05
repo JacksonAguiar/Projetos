cd60abd2dd9b8920295aaf4dccf8be4d3c872a40
Jacksonwsa35 <40011417+Jacksonwsa35@users.noreply.github.com>
2018-07-05 17:03:50 -0300

    Add files via upload

---
+++ COD_RELES.ino
@@ -0,0 +1,29 @@
+const int Rele1=7;
+const int Rele2=6;
+int botao = 5;
+int botao2 = 4;
+int estadobotao;
+int estadobotao2;
+void setup() {
+pinMode(Rele1, OUTPUT);
+pinMode(Rele2, OUTPUT);
+pinMode(botao ,INPUT);
+pinMode(botao2 ,INPUT);
+
+}
+
+void loop() {
+  estadobotao= digitalRead(botao);
+  estadobotao2= digitalRead(botao2);
+  
+  if (estadobotao==0){ 
+  digitalWrite(Rele1, HIGH);
+  }if (estadobotao2==0){
+  digitalWrite(Rele2, HIGH);
+  }if(estadobotao==1 ){
+    digitalWrite(Rele1, LOW);
+  }if(estadobotao2==1 ){
+    digitalWrite(Rele2, LOW);
+  }
+  
+}
